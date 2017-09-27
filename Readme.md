4dca513ab55c9d63233c1e9d35b4481c67d3c513
Kristian Mandrup <kmandrup@gmail.com>
2017-09-27 16:01:54 +0200

    initial

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Red UI
+
+Node-red UI for Vue 2.x.
+
+## Lerna project
+
+This is a [lerna](https://lernajs.io/) project.
+The project will contain multiple related packages that can be managed as a whole.
+
+See [Lerna Getting Started](https://lernajs.io/#getting-started) for typical development workflow.
+
+## Packages
+
+- `red-ui-common` Common UI components, used as essential building blocks for more complex UI components
+- ... more to follow
---
+++ packages/red-ui-common/src/components/CheckboxSet.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="red-ui-checkboxSet">
+  </div>
+</template>
+<script>
+import checkboxSet from '../controllers/checkboxSet'
+
+export default {
+  name: 'CheckboxSet',
+  created() {
+    let options = {}
+    this.$el.querySelector('.red-ui-checkboxSet').checkboxSet(options)
+  }
+}
+</script>
+
+<style lang="scss">
+// Extracted from SASS file in /assets folder of red-editor
+// How to use SCSS in Vue 2
+// https://medium.com/@mahesh.ks/using-sass-scss-in-vue-js-2-d472af0facf9
+.red-ui-checkboxSet {
+  width: 15px;
+  display: inline-block;
+  color: #888;
+  cursor: pointer;
+  input {
+    display: none;
+  }
+
+  &.disabled {
+    pointer-events: none;
+    color: #ddd;
+  }
+}
+</style>
